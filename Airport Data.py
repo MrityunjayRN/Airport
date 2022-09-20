e952dc14e8e8e72ea6efc4f34c8d6371980f0b8b
Mrityunjay <113609818+MrityunjayRN@users.noreply.github.com>
2022-09-20 18:05:00 +0530

    Add files via upload

---
+++ Airport Data.py
@@ -0,0 +1,10 @@
+Names= ({"Name":"Mrityunjay","Age":13,"Flight boarding Time":"10 am"},
+{"Name":"Pragyan","Age":12,"Flight boarding Time":"9 am"},
+{"Name":"Shanteri","Age":25,"Flight boarding Time":"9 am"},
+{"Name":"Anav","Age":15,"Flight boarding Time":"9 am"},
+{"Name":"Eklovya","Age":40,"Flight boarding Time":"10 am"},
+{"Name":"Shikha","Age":39,"Flight boarding Time":"10 am"},
+{"Name":"Shreya","Age":23,"Flight boarding Time":"10 am"},
+{"Name":"Shreshth","Age":22,"Flight boarding Time":"10 am"},
+{"Name":"Pooja","Age":63,"Flight boarding Time":"10 am"})
+
